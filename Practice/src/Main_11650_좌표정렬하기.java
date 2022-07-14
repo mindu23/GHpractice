2d23fb417c05f62c6d5ade353231eea99500b0cf
박민주 <minj5541@naver.com>
2022-07-14 20:02:18 +0900

    solved BOJ 좌표 정렬하기

---
+++ Practice/src/Main_11650_좌표정렬하기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_11650_좌표정렬하기 {
+
+    static class Point implements Comparable<Point>{
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            if (this.x == o.x) return Integer.compare(this.y, o.y);
+            return Integer.compare(this.x, o.x);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Point[] points = new Point[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            points[i] = new Point(x, y);
+        }
+
+        Arrays.sort(points);
+
+        for (Point point : points) {
+            System.out.println(point.x + " " + point.y);
+        }
+    }
+}
