e51d1d32d62070b64f2d17ceefd908bc83f3921b
minjuPark23 <minj5541@naver.com>
2022-09-08 23:45:40 +0900

    solved programmers 최댓값과 최솟값

---
+++ Practice/src/연습문제/D1_최댓값과최솟값.java
@@ -0,0 +1,34 @@
+package 연습문제;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class D1_최댓값과최솟값 {
+
+    @Test
+    public static void main(String[] args) {
+        Assertions.assertEquals(solution("1 2 3 4"), "1 4");
+    }
+
+    private static String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(s);
+        LinkedList<String> list = new LinkedList<>();
+
+        while(st.hasMoreTokens()){
+            list.add(st.nextToken());
+        }
+
+        Collections.sort(list);
+
+        answer.append(list.get(0)+" ");
+        answer.append(list.get(list.size()-1));
+        return answer.toString();
+    }
+}
