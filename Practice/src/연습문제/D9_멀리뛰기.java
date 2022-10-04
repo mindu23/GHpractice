bf137db3417e2d4b7a9b1316ae332923ea92efab
minjuPark23 <minj5541@naver.com>
2022-10-05 02:26:33 +0900

    solved programmers 멀리뛰기

---
+++ Practice/src/연습문제/D9_멀리뛰기.java
@@ -0,0 +1,23 @@
+package 연습문제;
+
+import org.junit.jupiter.api.Assertions;
+
+public class D9_멀리뛰기 {
+
+    public static void main(String[] args) {
+        Assertions.assertEquals(solution(4), 5);
+        Assertions.assertEquals(solution(3), 3);
+        Assertions.assertEquals(solution(1), 1);
+    }
+
+    private static long solution(int n) {
+        long answer = 0;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        if (n > 1) dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2])%1234567;
+        }
+        return dp[n];
+    }
+}
